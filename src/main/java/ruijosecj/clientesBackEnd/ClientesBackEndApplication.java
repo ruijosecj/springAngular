9fce37bed85f63973c5aead04664003685a60f9a
ruijosecj <ruijosecj@gmail.com>
2023-03-22 15:34:22 -0300

    Criando Projeto

---
+++ src/main/java/ruijosecj/clientesBackEnd/ClientesBackEndApplication.java
@@ -0,0 +1,13 @@
+package ruijosecj.clientesBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ruijosecj/clientesBackEnd/ClientesBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ruijosecj.clientesBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
